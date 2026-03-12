3673afbc2b3d784b344ae4048d2d144f8e2fa2ef
Thomas Weld <thomas.weld@gmail.com>
2026-03-11 23:23:11 -0400

    increase height

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -35,7 +35,7 @@ export default function TicketsPage() {
             widgetType: "checkout",
             eventId: EVENT_ID,
             iframeContainerId: CONTAINER_ID,
-            iframeContainerHeight: 1100,
+            iframeContainerHeight: 1800,
             onOrderComplete: () => {
               console.log("Order complete!");
             },
