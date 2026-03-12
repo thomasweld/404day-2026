c0689cef1f45503890b5a987311234373797fdc1
Thomas Weld <thomas.weld@gmail.com>
2026-03-12 12:07:54 -0400

    fix cors issue w/ vercel and sanity.io

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,19 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://*.sanity.studio https://sanity.studio",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
