8df3147d6d4be28e7c71eedb5a4d3b8af98a90af
Thomas Weld <thomas.weld@gmail.com>
2026-03-11 23:26:29 -0400

    fix height fr

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -21,9 +21,10 @@ export default function TicketsPage() {
       </section>
 
       {/* Eventbrite Widget */}
+      <style>{`#${CONTAINER_ID} iframe { min-height: 1800px !important; height: 1800px !important; }`}</style>
       <section className="py-20 px-4 sm:px-6 lg:px-8 bg-[#9ec367]">
         <div className="max-w-2xl mx-auto">
-          <div id={CONTAINER_ID} />
+          <div id={CONTAINER_ID} style={{ minHeight: "1800px" }} />
         </div>
       </section>
 
